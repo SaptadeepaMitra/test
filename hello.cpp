f2acf618a52233d7c836e9b7ab3802b0ac380229
Saptadeepa <saptadeepa.mitra2000@gmail.com>
2023-01-03 21:09:27 +0530

    change in the hello code

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
+	int n;
 	printf("hello");
+	printf("enter the value of n 1 or 0");
+	scanf("%d\n",&n);
+	if(n==1)
+	printf("one");
+	else
+	printf("zero");
 	return 0;
 }
